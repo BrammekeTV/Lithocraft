a1eb69b6c8354212f88b16e2100b7e1766f67a2e
Brammeke <brammekeofficial@gmail.com>
2021-07-05 12:59:42 +0200

    mixin update

--- src/main/java/net/brammeke/lithocraft/mixin/LithocraftMixin.java
+++ src/main/java/net/brammeke/lithocraft/mixin/LithocraftMixin.java
@@ -1,15 +1,13 @@
 package net.brammeke.lithocraft.mixin;
 
-import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.gui.screen.TitleScreen;
 
 @Mixin(TitleScreen.class)
 public class LithocraftMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		System.out.println("This line is printed by an example mod mixin!");
-	}
+	// @Inject(at = @At("HEAD"), method = "init()V")
+	// private void init(CallbackInfo info) {
+	// 	System.out.println("This line is printed by an example mod mixin!");
+	// }
 }
