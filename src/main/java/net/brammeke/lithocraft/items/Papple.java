623224325731aff9d4d20c6abd58ff6bd17a517e
Brammeke <brammekeofficial@gmail.com>
2021-07-08 11:47:43 +0200

    Trinkets

--- src/main/java/net/brammeke/lithocraft/items/Papple.java
+++ src/main/java/net/brammeke/lithocraft/items/Papple.java
@@ -1,16 +1,24 @@
 package net.brammeke.lithocraft.items;
 
-import java.util.List;
-
+import com.google.common.collect.Multimap;
+import dev.emi.trinkets.api.SlotAttributes;
+import dev.emi.trinkets.api.SlotReference;
+import dev.emi.trinkets.api.TrinketItem;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.item.TooltipContext;
-import net.minecraft.item.Item;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.world.World;
 
-public class Papple extends Item  {
+import java.util.List;
+import java.util.UUID;
+
+public class Papple extends TrinketItem {
     public Papple(Settings settings) {
         super(settings);
     }
@@ -29,12 +37,12 @@ public class Papple extends Item  {
         }
     }
 
-    // public Multimap<EntityAttribute, EntityAttributeModifier> getModifiers(ItemStack stack, SlotReference slot, LivingEntity entity, UUID uuid) {
-    //     var modifiers = super.getModifiers(stack, slot, entity, uuid);
-    //     // +10% movement speed
-    //     modifiers.put(EntityAttributes.GENERIC_MOVEMENT_SPEED, new EntityAttributeModifier(uuid, "guidemod:movement_speed", 0.1, EntityAttributeModifier.Operation.MULTIPLY_TOTAL));
-    //     // If the player has access to ring slots, this will give them an extra one
-    //     SlotAttributes.addSlotModifier(modifiers, "hand/ring", uuid, 1, EntityAttributeModifier.Operation.ADDITION);
-    //     return modifiers;
-    // }
+     public Multimap<EntityAttribute, EntityAttributeModifier> getModifiers(ItemStack stack, SlotReference slot, LivingEntity entity, UUID uuid) {
+         var modifiers = super.getModifiers(stack, slot, entity, uuid);
+         // +10% movement speed
+         modifiers.put(EntityAttributes.GENERIC_MOVEMENT_SPEED, new EntityAttributeModifier(uuid, "guidemod:movement_speed", 0.1, EntityAttributeModifier.Operation.MULTIPLY_TOTAL));
+         // If the player has access to ring slots, this will give them an extra one
+         SlotAttributes.addSlotModifier(modifiers, "hand/ring", uuid, 1, EntityAttributeModifier.Operation.ADDITION);
+         return modifiers;
+     }
 }
